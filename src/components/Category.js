ff6b0b8ec1c7180fa135c4d8414ef6de8e7c47f2
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-10-07 11:47:04 +0300

    3 lesson  commit

--- src/components/Category.js
+++ src/components/Category.js
@@ -11,7 +11,8 @@ function Category({items, onClickCategory}) {
     <div className="categories">
         <ul>
             <li className={activeItem === null ? "active" : ''} onClick ={() => onSelectItem(null) }> Все</li>
-            {items.map((name, index) => (
+            {items &&
+            items.map((name, index) => (
               <li className={activeItem === index ? "active" : ''}
                onClick = {()=> onSelectItem(index)}
                 key ={`${name}_${index}`}>{name}</li>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Button from './Button';
 import Header from './Header';
 import Category from './Category';
+import SortPopup from './SortPopup';
 
-
-export {Button, Header, Category}
+export {Button, Header, Category, SortPopup}
